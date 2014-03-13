5a106a8bdcde48abd5a4e78c5ea5b50aabfa6502
Jeremy Geros <jeremy453@gmail.com>
2014-03-13 12:14:48 -0600

    tile class

---
+++ src/com/jeremygeros/level/tile/Tile.java
@@ -0,0 +1,23 @@
+package com.jeremygeros.level.tile;
+
+import com.jeremygeros.graphics.Screen;
+import com.jeremygeros.graphics.Sprite;
+
+public class Tile {
+	
+	public int x, y;
+	public Sprite sprite;
+	
+	public Tile(Sprite sprite) {
+		this.sprite = sprite;
+	}
+	
+	public void render(int x, int y, Screen screen) {
+		
+	}
+	
+	public boolean solid() {
+		return false;
+	}
+	
+}
